10bee9d2502f3f79f4cf670be0fb5b7041213862
pliffisson <pliffissongomes@gmail.com>
2020-07-01 15:40:08 -0400

    instalaçao e configuração do nunjucks

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const server = express();
 
+// ============ Carregando a estilização da pagina ===============
+server.use(express.static("public"));
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+  express: server,
+});
+
+// ======= Fazendo a rota do servidor ========
+server.get("/", function (req, res) {
+  return res.render("about");
+});
+server.get("/portfolio", function (req, res) {
+  return res.render("portfolio");
+});
+
+// ======== Servidor ===========
 server.listen(3000, function () {
   console.log("server is running");
 });

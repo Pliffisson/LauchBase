e6af2b5e64834c7e59dd888c9fbf0ff9c21062be
pliffisson <pliffissongomes@gmail.com>
2020-07-03 16:21:04 -0400

    Atualização dos arquivos com informações de forma dinamica

---
+++ Backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    id: "AJ5lW2yS0Fs",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+  {
+    id: "BnmG0IftEQ4",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+  {
+    id: "AkmK-JG9hKM",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+  {
+    id: "E45YQdLQBY4",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+  {
+    id: "jOFctNhY8-Q",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+  {
+    id: "I0RBmyN2euU",
+    title: "HD Breakout - 2016 Upgrades",
+    duration: "6:03 Minutos",
+    price: "Free",
+  },
+];
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,9 @@ const nunjucks = require("nunjucks");
 
 const server = express();
 
+// Exportando o modulo de data.js
+const videos = require("./data");
+
 // ============ Carregando a estilização da pagina ===============
 server.use(express.static("public"));
 
@@ -14,10 +17,27 @@ nunjucks.configure("views", {
 
 // ======= Fazendo a rota do servidor ========
 server.get("/", function (req, res) {
-  return res.render("about");
+  const about = {
+    avatar_url:
+      "https://avatars0.githubusercontent.com/u/8752955?s=460&u=1b7d0e9bd1ca441996d9e3734942dd6119db0e6e&v=4",
+    name: "Pliffisson Gomes",
+    role: "Frontend Developer",
+    description:
+      "Developer Frontend focusing on web and mobile development using, html, css, javascript, node, react and react native technologies.",
+    links: [
+      { name: "Github", url: "https://github.com/pliffisson" },
+      {
+        name: "Linkedin",
+        url: "https://linkedin.com/in/pliffisson-gomes-622b3551",
+      },
+    ],
+  };
+
+  return res.render("about", { about });
 });
+
 server.get("/portfolio", function (req, res) {
-  return res.render("portfolio");
+  return res.render("portfolio", { items: videos });
 });
 
 // ======== Servidor ===========

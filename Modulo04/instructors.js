0bb3a75607d969eec632103f7e239c2467d16a93
pliffisson <pliffissongomes@gmail.com>
2020-08-24 00:02:52 -0400

    Trabalhando com dados em JSON

--- Modulo04/instructors.js
+++ Modulo04/instructors.js
@@ -1,5 +1,7 @@
-// Create
+const fs = require("fs");
+const data = require("./data.json");
 
+// Create
 exports.post = function (req, res) {
   const keys = Object.keys(req.body);
 
@@ -8,5 +10,14 @@ exports.post = function (req, res) {
       return res.send("Preencha todos os campos");
     }
   }
-  return res.send(req.body);
+
+  data.instructors.push(req.body);
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send("Error na gravação do arquivo");
+
+    return res.redirect("/instructors");
+  });
+
+  //   return res.send(req.body);
 };

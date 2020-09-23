c21b04ed0248eb9d8085d76b0fb06ddfb51ffe5a
pliffisson <pliffissongomes@gmail.com>
2020-09-23 15:39:36 -0400

    Apresentaçao dos membros

--- Modulo04/controllers/members.js
+++ Modulo04/controllers/members.js
@@ -7,24 +7,6 @@ exports.index = function (req, res) {
   return res.render("members/index", { members: data.members });
 };
 
-// show
-exports.show = function (req, res) {
-  const { id } = req.params;
-
-  const foundMember = data.members.find(function (member) {
-    return member.id == id;
-  });
-
-  if (!foundMember) return res.send("Instrutor não encontrado");
-
-  const member = {
-    ...foundMember,
-    age: age(foundMember.birth),
-  };
-
-  return res.render("members/show", { member });
-};
-
 exports.create = function (req, res) {
   return res.render("members/create");
 };
@@ -62,6 +44,24 @@ exports.post = function (req, res) {
   });
 
   //   return res.send(req.body);
+};
+
+// show
+exports.show = function (req, res) {
+  const { id } = req.params;
+
+  const foundMember = data.members.find(function (member) {
+    return member.id == id;
+  });
+
+  if (!foundMember) return res.send("Instrutor não encontrado");
+
+  const member = {
+    ...foundMember,
+    age: age(foundMember.birth),
+  };
+
+  return res.render("members/show", { member });
 };
 
 // Edit
